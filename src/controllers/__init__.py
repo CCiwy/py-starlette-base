169691a191432bc7b233ed8a92299c0a651c4e44
Ciwy <c.walter@afk-dev.de>
2023-05-02 17:58:11 +0200

    add: controller module exports active controllers

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,2 +1,8 @@
 from src.controllers.base import BaseController
 from src.controllers.base import Controller
+
+# todo: this is only for debug purpose. remove later!
+from src.controllers.debug import DebugController
+
+
+CONTROLLERS = [DebugController]
