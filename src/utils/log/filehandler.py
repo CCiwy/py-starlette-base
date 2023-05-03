b5c33ed34b2faaa38d0ed3524d38ae161a506c95
Ciwy <c.walter@afk-dev.de>
2023-05-03 12:54:11 +0200

    fix: filehandler not added as handler to logger

--- src/utils/log/filehandler.py
+++ src/utils/log/filehandler.py
@@ -9,23 +9,32 @@ TIME_ROTATING_WHEN = 'midnight' # todo: get from conf
 LOG_BACKUP_COUNT = 7 # todo: get from conf
 
 
-def create_file_logger(app_name, log_dir):
+def create_file_logger(app):
+    app_name = app.config.APP_NAME
     if not len(app_name):
         # app name not set in conf
         app_name = 'app'
+    
+    log_dir = app.config.LOG_DIR
     log_path = f'{log_dir}/{app_name.replace(" ", "_")}.log'
+    
+    time_rotate_when = app.config.TIME_ROTATING_WHEN
+    log_backup_count = app.config.LOG_BACKUP_COUNT
+    
     file_handler = TimedRotatingFileHandler(
         log_path,
-        when=TIME_ROTATING_WHEN,
+        when=time_rotate_when,
         interval=1,
-        backupCount=LOG_BACKUP_COUNT
+        backupCount=log_backup_count
     )
 
 
+    _logger = logging.Logger(app_name)
     file_handler.setFormatter(logging.Formatter(DEFAULT_FORMAT))
-    file_handler.setLevel(logging.DEBUG)
+    
+    _logger.addHandler(file_handler)
+    file_handler.setLevel(logging.INFO)
 
-    # todo: identify streamhandler
-
-
+    
+    return file_handler
 
