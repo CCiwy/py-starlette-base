4f4092c909e126ac5bff0f27df5cb6d3ca99a878
Ciwy <c.walter@afk-dev.de>
2023-05-03 11:07:43 +0200

    add: config.dev ADMIN_MAIL

--- config/development.py
+++ config/development.py
@@ -1,5 +1,7 @@
 from config.default import DB_LOCATION
+from src.mail import ADMIN_MAIL
 
 DEBUG = True
 APP_NAME = 'TEST'
 SQLALCHEMY_DATABASE_URI = DB_LOCATION # lol?
+ADMIN_MAIL = 'test@mail.lol'
