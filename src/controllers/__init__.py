96f15f2a848a1af0973f42e9ba9e52d66605ee63
Ciwy <c.walter@afk-dev.de>
2023-05-04 14:22:02 +0200

    controller module exports UserController

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,8 +1,14 @@
 from src.controllers.base import BaseController
 from src.controllers.base import Controller
 
+<<<<<<< Updated upstream
 # todo: this is only for debug purpose. remove later!
 from src.controllers.debug import DebugController
 
 
 CONTROLLERS = [DebugController]
+=======
+from src.controllers.user import UserController
+
+CONTROLLERS = [UserController]
+>>>>>>> Stashed changes
