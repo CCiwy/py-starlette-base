923fca13aeea2c34373d621124523a2dcaded4ab
Ciwy <c.walter@afk-dev.de>
2023-05-04 17:23:49 +0200

    add: UserController create

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
 # Import Home-Grown
 from src.controllers.base import BaseController
 from src.schemas.user import TokenRequestSchema
-
+from src.schemas.user import UserCreateSchema
 
 from src.utils.log.requests import request_logger
 
@@ -18,7 +18,9 @@ from enum import Enum
 class UserError(str, Enum):
     AUTH_INCOMPLETE = 'Authdata incomplete'
     AUTH_INCORRECT = 'Authdata incorrect'
-
+    NAME_NOT_FREE = 'Username is already in use'
+    PASSWORDS_DONT_MATCH = 'Passwords dont match'
+        
     def __repr__(self):
         return self.value
 
@@ -33,10 +35,40 @@ class UserController(BaseController):
     def make_routes(self):
         path_base = "/user"
         routes = [
-            ("/token", "POST", self.get_token)
+            ("/token", "POST", self.get_token),
+            ("/create", "POST", self.create_user)
         ]
 
         return path_base, routes
+
+    async def create_user(self, request):
+        data = await self.deserialize_request(request, schema=UserCreateSchema)
+        
+        user_name = data.get('user_name')
+        password = data.get('password')
+        password_repeat = data.get('password_repeat')
+
+        if not (user_name and password and password_repeat):
+            # should never happen without request deserialize error
+            #return self.bad_request(UserError.AUTH_INCOMPLETE)
+            return self.response(UserError.AUTH_INCOMPLETE, 403)
+
+
+        if not password == password_repeat:
+            return self.response(UserError.PASSWORDS_DONT_MATCH, 403)
+        
+
+        user_service = self.app.get_service("user")
+
+        user_exists =  user_service.get_user("user_name")
+        if user_exists:
+            # check what status code is the proper one to return here
+            return self.response(UserError.NAME_NOT_FREE, 403)
+
+        user_service.create_user(user_name, password)
+       
+
+        return self.response('Account created', 201)
 
     @request_logger('base')
     async def get_token(self, request):
