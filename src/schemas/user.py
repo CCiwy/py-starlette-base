f1a64d4940af07c52ce3685382ae896ac45828b0
Ciwy <c.walter@afk-dev.de>
2023-05-04 17:21:27 +0200

    add: schemas.user UserCreateSchema

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,6 +1,14 @@
 from marshmallow.schema import Schema
 from marshmallow.fields import String
 
+
+class UserCreateSchema(Schema):
+    user_name = String(required=True)
+    password = String(required=True)
+    password_repeat = String(required=True)
+
+
+
 class TokenRequestSchema(Schema):
     user_name = String(required=True)
     password = String(required=True)
