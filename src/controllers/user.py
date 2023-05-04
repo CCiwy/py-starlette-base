067b0529ee962bca6f47e7fa1453b66c0ada8c1f
Ciwy <c.walter@afk-dev.de>
2023-05-04 14:30:31 +0200

    add: usercontroller.get_token, TokenRequestSchema

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,6 +1,16 @@
 # Import Home-Grown
+from typing import Required
 from src.controllers.base import BaseController
 from src.utils.log.requests import request_logger
+
+
+from marshmallow.schema import Schema
+from marshmallow.fields import String
+
+class TokenRequestSchema(Schema):
+    user_name = String(required=True)
+    password = String(required=True)
+
 
 class UserController(BaseController):
 
@@ -11,3 +21,8 @@ class UserController(BaseController):
         routes = []
 
         return path_base, routes
+
+
+    async def get_token(self, request):
+        token = 'asdf'
+        return self.response(token, 200)
