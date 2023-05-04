69ddb5f9193340824d981389cf1abbe360f245b7
Ciwy <c.walter@afk-dev.de>
2023-05-04 14:00:48 +0200

    code maintenance: main branch has no controllers

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,8 +1,4 @@
 from src.controllers.base import BaseController
 from src.controllers.base import Controller
 
-# todo: this is only for debug purpose. remove later!
-from src.controllers.debug import DebugController
-
-
-CONTROLLERS = [DebugController]
+CONTROLLERS = []
