d74db47688d6bf711a03de5d4b31bf99e26676e0
Ciwy <c.walter@afk-dev.de>
2023-05-08 16:31:04 +0200

    add: UserService.start_session,user_exists + cleanup file

--- src/services/user.py
+++ src/services/user.py
@@ -1,44 +1,72 @@
-from src.services.base import BaseService
+# Import Built-Ins
 
 
-
-def hash_password(password):
-    # todo: implement this
-    return password
-
-def create_token():
-    return 'asdf'
+# Import Third-Party
+from sqlalchemy import select
+from sqlalchemy.sql.functions import user
 
 
-class UserModel:
-    # todo: create this as Basemodel instance
-    def __init__(self, name, password):
-        self.name = name
-        self.salted_pass = hash_password(password)
-        self.token = False # delete after implementing as BaseModel
+# Import Home-Grown
+from src.database.session import DatabaseService
+from src.database.session import LookUpError
 
 
-    def start_session(self):
-        self.token = create_token()
-        # add expiration timer
-        return self.token
+from src.database.models.user import UserModel
 
-class UserService(BaseService):
+class UserService(DatabaseService):
     instance_name = 'user'
-    users = dict()
+    _model = UserModel
 
 
-    def verifiy_password(self, user: UserModel, password: str) -> bool:
-        hashed = hash_password(password)
-        return hashed == user.salted_pass
+    async def get_by_uuid(self, uuid):
+        stmt = select(self._model).where(self._model.uuid == uuid)
+        async with self.db.session as session:
+            result = await session.execute(stmt)
+            model = result.scalar_one_or_none()
+            if model is None:
+                return self._status_error(LookUpError(uuid, self.instance_name))
+            
+
+            return self._status_ok(model)
 
 
-    def get_user(self, user_name):
-        return self.users.get(user_name, False)
+    async def start_session(self, uuid):
+        stmt = select(self._model).where(self._model.uuid == uuid)
+
+        async with self.db.session as session:
+            result = await session.execute(stmt)
+            user = result.scalar_one_or_none()
+            if user is None:
+                return
+
+            token = user.start_session()
+            await session.commit()
+            return token
+
+    async def user_exists(self, user_name):
+        user = await self._get_user(user_name)
+        return user is not None
 
 
+    async def get_user(self, user_name):
+        model = await self._get_user(user_name)
+        if model is None:
+            return self._status_error(LookUpError(user_name, self.instance_name))
 
-    def create_user(self, user_name, password):
-        self.users[user_name] = UserModel(user_name, password)
+        return model
+
+
+    async def _get_user(self, user_name): 
+        stmt = select(self._model).where(self._model.name == user_name)
+        async with self.db.session as session:
+            result = await session.execute(stmt)
+            model = result.scalar_one_or_none()
+            return model
+
+
+    async def create_user(self, user_name, password):
+        user_model = UserModel(user_name, password)
+
+        return await self.save(user_model)
 
 
