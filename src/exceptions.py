35ec7cd36290d70e691f2b5ed35f64e9fb526f3d
Ciwy <c.walter@afk-dev.de>
2023-05-08 16:42:39 +0200

    fix: exceptions were not inheriting from BaseError

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,15 +1,36 @@
-class RequestError(RuntimeError):
-    pass
+class ErrorBase(RuntimeError):
 
+    def __init__(self, detail='', source=None):
+        self.detail = detail
+        self.source = detail
 
+<<<<<<< Updated upstream
 class DeserializeError(RuntimeError):
     status = 400
+=======
+>>>>>>> Stashed changes
     @classmethod
     def init_from(cls, error):
         detail = getattr(error, 'message', repr(error))
         return cls(detail)
 
 
-class DataBaseError(RuntimeError):
+    def __repr__(self):
+        return f'{self.__class__.__name__} {self.detail}'
+
+
+class RequestError(ErrorBase):
     pass
+
+
+class Unauthorized(RequestError):
+    status = 403
+
+
+class DeserializeError(ErrorBase):
+    status = 400
+
+
+class DataBaseError(ErrorBase):
+    status = 500
 
