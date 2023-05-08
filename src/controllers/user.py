90f1a6ebb7252e0596d6eba56fbe2c286d0ccc78
Ciwy <c.walter@afk-dev.de>
2023-05-08 16:33:55 +0200

    add: UserController.token/profile + cleanup file

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,5 +1,6 @@
 # Import Built-Ins
 from typing import TYPE_CHECKING
+
 # Import Third-Party
 if TYPE_CHECKING:
    from marshmallow.schema import Schema
@@ -7,25 +8,19 @@ if TYPE_CHECKING:
 
 # Import Home-Grown
 from src.controllers.base import BaseController
+from src.database.session import DBResult, DBStatus
+
+from src.error_codes import UserError
+
 from src.schemas.user import TokenRequestSchema
 from src.schemas.user import UserCreateSchema
+from src.schemas.user import UserProfileSchema
+
+from src.utils.auth import authenticated
 
 from src.utils.log.requests import request_logger
-
-# todo: move this to seperate file:
-from enum import Enum
-
-class UserError(str, Enum):
-    AUTH_INCOMPLETE = 'Authdata incomplete'
-    AUTH_INCORRECT = 'Authdata incorrect'
-    NAME_NOT_FREE = 'Username is already in use'
-    PASSWORDS_DONT_MATCH = 'Passwords dont match'
-        
-    def __repr__(self):
-        return self.value
-
-
-
+from src.utils.log import get_logger
+logger = get_logger('CTRL')
 
 
 class UserController(BaseController):
@@ -36,14 +31,16 @@ class UserController(BaseController):
         path_base = "/user"
         routes = [
             ("/token", "POST", self.get_token),
-            ("/create", "POST", self.create_user)
+            ("/create", "POST", self.create_user),
+            ("/profile", "GET", self.my_user_data)
         ]
 
         return path_base, routes
 
+
+    #@request_logger('base')
     async def create_user(self, request):
         data = await self.deserialize_request(request, schema=UserCreateSchema)
-        
         user_name = data.get('user_name')
         password = data.get('password')
         password_repeat = data.get('password_repeat')
@@ -51,26 +48,31 @@ class UserController(BaseController):
         if not (user_name and password and password_repeat):
             # should never happen without request deserialize error
             #return self.bad_request(UserError.AUTH_INCOMPLETE)
-            return self.response(UserError.AUTH_INCOMPLETE, 403)
+            return self.bad_request(UserError.AUTH_INCOMPLETE)
 
 
         if not password == password_repeat:
-            return self.response(UserError.PASSWORDS_DONT_MATCH, 403)
+            return self.bad_request(UserError.PASSWORDS_DONT_MATCH)
         
 
         user_service = self.app.get_service("user")
 
-        user_exists =  user_service.get_user("user_name")
+        
+        user_exists = await user_service.user_exists(user_name)
         if user_exists:
             # check what status code is the proper one to return here
-            return self.response(UserError.NAME_NOT_FREE, 403)
+            return self.bad_request(UserError.NAME_NOT_FREE)
 
-        user_service.create_user(user_name, password)
+        result = await user_service.create_user(user_name, password)
+
+        if not result.status == DBStatus.OK:
+            return self.internal_error('sth went wrong')
        
 
         return self.response('Account created', 201)
 
-    @request_logger('base')
+
+    #@request_logger('base')
     async def get_token(self, request):
         # deserialize request
         data = await self.deserialize_request(request, schema=TokenRequestSchema)
@@ -78,20 +80,29 @@ class UserController(BaseController):
         password = data.get('password')
         if not (user_name and password):
             # should never happen without request deserialize error
-            #return self.bad_request(UserError.AUTH_INCOMPLETE)
+            return self.bad_request(UserError.AUTH_INCOMPLETE)
         
-            return self.response(UserError.AUTH_INCOMPLETE, 403)
 
         # call user_service
         user_service = self.app.get_service("user")
-        user_model = user_service.get_user(user_name)
+        user_model = await user_service.get_user(user_name)
         # check if user exist + password matches
-        if not user_model or not user_service.verifiy_password(user_model, password):
-            return self.response(UserError.AUTH_INCORRECT, 403)
-            #return self.unauthorized(UserError.AUTH_INCORRECT) 
-        # todo: this will be async methods after implementing service correctly
+        if not user_model or not user_model.verifiy_password(password):
+            return self.unauthorized(UserError.AUTH_INCORRECT) 
     
         # start session, get token
-        token = user_model.start_session()
+        token = await user_service.start_session(user_model.uuid)
 
         return self.response(token, 200)
+
+    @request_logger('base')
+    @authenticated('base')
+    async def my_user_data(self, request, user=False):
+
+        logger.debug(f'USER: {user}')
+        if not user:
+            return self.unauthorized('not authenticated')
+
+        logger.debug('should call json response')
+        return self.json_response(data=user, schema=UserProfileSchema, status=200)
+
